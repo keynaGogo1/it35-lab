c2520845cfea1645bf225c3631dafa92c4f63c6a
keynaGogo1 <keynagogo5@gmail.com>
2025-02-27 14:21:29 +0800

    modified package, package-lock, login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,12 @@
 
 import { 
+  IonAvatar,
     IonButton,
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonInput, 
+      IonInputPasswordToggle, 
       IonMenuButton, 
       IonPage, 
       IonTitle, 
@@ -23,7 +26,14 @@ import {
           <IonToolbar>
             <IonTitle>Login</IonTitle>
           </IonToolbar>
-        </IonHeader>
+        </IonHeader> 
+        <IonAvatar>
+            <img alt="person-outline" src="https://ionic.io/ionicons"/>
+        </IonAvatar>
+       
+        <IonInput type="password" label="Password" value="">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+    </IonInput>
         <IonContent className='ion-padding'>
             <IonButton onClick={() => doLogin()} expand="full">
                 Login

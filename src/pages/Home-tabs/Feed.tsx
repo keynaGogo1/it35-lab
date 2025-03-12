eb02ee16546f35154c56cc76f116eb53ca04762d
keynaGogo1 <keynagogo5@gmail.com>
2025-03-12 18:17:07 +0800

    updated Feed.tsx

--- src/pages/Home-tabs/Feed.tsx
+++ src/pages/Home-tabs/Feed.tsx
@@ -24,33 +24,41 @@ import {
             <IonTitle>Feed</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonContent fullscreen>
         <IonCard>
       <IonCardHeader>
-        <IonCardTitle></IonCardTitle>
+        <IonCardTitle>Card Title</IonCardTitle>
         <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+    <IonCard>
+      <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1564410267841-915d8e4d71ea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cXVvdGV8ZW58MHx8MHx8fDA%3D" />
+      <IonCardHeader>
+        <IonCardTitle>Remember why you started</IonCardTitle>
+        <IonCardSubtitle></IonCardSubtitle>
       </IonCardHeader>
 
       <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
     </IonCard>
     <IonCard>
-      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1605514449459-5a9cfa0b9955?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHF1b3RlfGVufDB8fDB8fHww" />
       <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        <IonCardTitle>if You Never Know failure You Will Never Know Success</IonCardTitle>
+        <IonCardSubtitle></IonCardSubtitle>
       </IonCardHeader>
 
       <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
     </IonCard>
     <IonCard>
-      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <img alt="Silhouette of mountains" src="https://images.unsplash.com/photo-1577640256262-8488aa247e17?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHF1b3RlfGVufDB8fDB8fHww" />
       <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        <IonCardTitle>Wake Up Kick Ass Be Kind Repeat</IonCardTitle>
+        <IonCardSubtitle></IonCardSubtitle>
       </IonCardHeader>
 
       <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
     </IonCard>
-        <IonContent fullscreen>
           <div
             style={{
               display: 'flex',

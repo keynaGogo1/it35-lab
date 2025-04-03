ca4ccbd953b2773934dee7d625b56f5b7e5c34e9
RayonB <rinevarayon@gmail.com>
2025-04-03 14:25:54 +0800

    Updates

--- assets/keyboard2-legacy-jM25gfTp.js
+++ assets/keyboard2-legacy-jM25gfTp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DzokCf0a.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CnKyIzZ_.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

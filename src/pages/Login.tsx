d0477be1a42555abb3888dcb8a667bdc7e2e5e9c
keynaGogo1 <keynagogo5@gmail.com>
2025-04-03 22:11:15 +0800

    updated template/menu/login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,72 @@
-
+import React, { useState } from 'react';
 import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+  IonButton,
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonInput,
+  IonItem,
+  IonLabel,
+  useIonRouter,
+  IonAlert
+} from '@ionic/react';
+import './Login.css'; 
 
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
+
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+
+  const doLogin = () => {
+    if (username === '' || password === '') {
+      setShowAlert(true);
+    } else {
+      navigation.push('/it35-lab/app', 'forward', 'replace');
     }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding background-image' id= "Login">
+        <IonItem>
+          <IonLabel position="floating">Username</IonLabel>
+          <IonInput 
+            value={username} 
+            onIonChange={e => setUsername(e.detail.value!)} 
+            required 
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Password</IonLabel>
+          <IonInput 
+            type="password" 
+            value={password} 
+            onIonChange={e => setPassword(e.detail.value!)} 
+            required 
+          />
+        </IonItem>
+        <IonButton onClick={doLogin} expand="full" className="ion-margin-top">
+          Login
+        </IonButton>
+        <IonAlert
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          header={'Error'}
+          message={'Please enter both username and password.'}
+          buttons={['OK']}
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;

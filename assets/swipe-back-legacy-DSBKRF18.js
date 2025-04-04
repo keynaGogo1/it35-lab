d7f9c591d5bb08c9a638ab4b3bc1498cda0c6752
keynaGogo1 <keynagogo5@gmail.com>
2025-04-04 18:00:42 +0800

    Updates

--- assets/swipe-back-legacy-DSBKRF18.js
+++ assets/swipe-back-legacy-DSBKRF18.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BWZne29t.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DjbBhHYV.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
